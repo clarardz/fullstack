8e56769972c015bdf67117188d4562eb764c9ff8
clarardz <clararomerodiaz98@gmail.com>
2023-02-06 14:37:27 +0100

    js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
-
-alert("Hello world!"); 
-document.write("Hello world!"); 
-console.log("Hello world!") 
-
+console.log("Hello World"); 
+console.log("a ver si funciona")
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha');
+document.getElementById("demo").innerHTML = "hola"; 
---
+++ JS/variables.js
@@ -0,0 +1,5 @@
+var clara = "guapa"; 
+console.log(clara); 
+document.write(clara); 
+var clara="diosa"; 
+document.write(clara); 
